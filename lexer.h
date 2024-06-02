2143f536b0ed954a8cf1dae4d2c6e8e8e5bf90b2
sayori1 <ridickeon@yandex.com>
2024-06-02 21:27:39 +0500

    refactoring

---
+++ lexer.h
@@ -0,0 +1,8 @@
+//
+// Created by almetoff on 02.06.2024.
+//
+
+#ifndef UNTITLED4_LEXER_H
+#define UNTITLED4_LEXER_H
+
+#endif //UNTITLED4_LEXER_H
---
+++ parser.h
@@ -0,0 +1,8 @@
+//
+// Created by almetoff on 02.06.2024.
+//
+
+#ifndef UNTITLED4_PARSER_H
+#define UNTITLED4_PARSER_H
+
+#endif //UNTITLED4_PARSER_H
